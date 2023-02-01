d895b147190a24808658771f625965157362efd0
lilluv <namdohai.hust@gmail.com>
2023-02-01 09:45:32 +0700

    Update task 2.3

--- task_2/task_2.3/dust_process.py
+++ task_2/task_2.3/dust_process.py
@@ -1,25 +1,41 @@
 import pandas as pd
 import argparse
+import datetime
 # task 2.3
 def main(args):
     summary = [['id', 'parameters', 'time', 'values']]
     #Read data
     df = pd.read_csv(args.path)
     sensors = list(set(df['id']))
-    log = {x: [] for x in sensors}
+    logs = {x: [] for x in sensors}
     for id, time, value in zip(df['id'], df['time'], df['values']):
-        log[id].append(value)
+        logs[id].append([value,time])
     for sensor in sensors:
-        values = log[sensor]
-        min_value = min(values)
-        max_value = max(values)
-        mean_value = sum(values)/len(values)
-        summary.append([sensor, 'max', 'none', max_value])
-        summary.append([sensor, 'min', 'none', min_value])
-        summary.append([sensor, 'mean', 'none', mean_value])
+        values = logs[sensor]
+        max_value = 0
+        time_max = 0
+        min_value = 551 # >550.5 is outlier
+        time_min = 0
+        sum_val = 0 
+        num_rec = 0
+        for value in values:
+            num_rec+=1
+            sum_val+=value[0]
+            if value[0] > max_value:
+                max_value = value[0]
+                time_max = value[1]
+            if value[0] < min_value:
+                min_value = value[0]
+                time_min = value[1]
+        mean_value = sum_val/num_rec
+        record_time = values[-1][1]
+        summary.append([sensor, 'max', time_max, max_value])
+        summary.append([sensor, 'min', time_min, min_value])
+        summary.append([sensor, 'mean', record_time, mean_value])
     # summary     
     summary = pd.DataFrame(summary)
-    pd.DataFrame.to_csv(summary, "dust_summary.csv", header= False, index= False )
+    pd.DataFrame.to_csv(summary, "dust_summary.csv", header= False, index= False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process.')
